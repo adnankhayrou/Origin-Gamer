5465d1097994d5e072c5303df13100900e666182
adnankhayrou <adnanekhayrou3@gmail.com>
2022-11-16 12:05:41 +0100

    adding header and navbar on navbar.php file

---
+++ navbar.php
@@ -0,0 +1,22 @@
+//header and navbar
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/vendor.min.css">
+    <link rel="stylesheet" href="css/default/app.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title><?php echo $title ?></title>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-black fixed-top">
+
+    <div class="container-fluid">
+      <a class="navbar-brand mx-2" href="#">ORIGIN GAMER</a>
+     </div>
+  </nav>
